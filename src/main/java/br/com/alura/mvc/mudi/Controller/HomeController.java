790ed5703d16a3281169debad06a05c1b844fa09
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-08-06 23:16:39 -0300

    Percorrendo a List View

--- src/main/java/br/com/alura/mvc/mudi/Controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/Controller/HomeController.java
@@ -1,8 +1,14 @@
 package br.com.alura.mvc.mudi.Controller;
 
+import br.com.alura.mvc.mudi.Models.Pedido;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -10,10 +16,21 @@ public class HomeController {
     @GetMapping("/home")
     public String home(Model model){
 
-        model.addAttribute("nomeProduto", "");
-        model.addAttribute("valorEntrega","");
-        model.addAttribute("dataEntrega","");
-        model.addAttribute("urlProduto","");
+        Pedido pedido = new Pedido();
+
+        pedido.setNomeProduto(" PIKACHU ACTION FIGURE ");
+
+        pedido.setDataDaEntrega(LocalDate.now());
+
+        pedido.setUrlProduto("https://www.ebay.com/p/606526822?iid=293631670005");
+
+        pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/71vc5ySs41L._AC_SL1500_.jpg");
+
+        pedido.setDescricao(" PIKACHU EM FORMA DE BONECO ");
+
+         List<Pedido> pedidos = Arrays.asList(pedido);
+
+         model.addAttribute("pedidos", pedidos);
 
         return "home";
 
