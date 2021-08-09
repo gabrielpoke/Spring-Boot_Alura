361a3911113c9825d551c93fb27fd8b43b6c39d6
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-08-09 17:32:09 -0300

    Usando JPA puro

--- src/main/java/br/com/alura/mvc/mudi/Controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/Controller/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -13,22 +16,14 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/home")
     public String home(Model model){
 
-        Pedido pedido = new Pedido();
-
-        pedido.setNomeProduto(" PIKACHU ACTION FIGURE ");
-
-        pedido.setDataDaEntrega(LocalDate.now());
-
-        pedido.setUrlProduto("https://www.ebay.com/p/606526822?iid=293631670005");
-
-        pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/71vc5ySs41L._AC_SL1500_.jpg");
-
-        pedido.setDescricao(" PIKACHU EM FORMA DE BONECO ");
-
-         List<Pedido> pedidos = Arrays.asList(pedido);
+        Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+        List<Pedido> pedidos = query.getResultList();
 
          model.addAttribute("pedidos", pedidos);
 
